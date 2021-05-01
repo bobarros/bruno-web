9f0c0c98a7875958031b1f15235ffecc2a3367de
Bruno Barros <bobarros@gmail.com>
2021-05-01 11:00:51 -0300

    removing gm tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,7 @@
 import "styles/index/main.scss";
-import TagManager from "react-gtm-module";
 
-function MyApp(props) {
-  const tagManagerArgs = {
-    gtmId: "GTM-W9N54M7",
-  };
-
+function MyApp(props:any) {  
   const { Component, pageProps } = props;
-  if (process.browser) {
-    TagManager.initialize(tagManagerArgs);
-  }
   return (
     <>
       <Component {...pageProps} />
